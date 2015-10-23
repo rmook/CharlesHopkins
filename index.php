7a7e22e4c4616cc7e57483da4e87b780f7e9c190
rmook <richard.mook@gmail.com>
2015-10-23 14:25:39 -0400

    modifications to banner; adding external link icon

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ include(ROOT_PATH . '/inc/header.php');
 		<div class="left-image">
 			<img src="img/C73C2723.jpg">
 		</div>
-		<div class="right-logo">
-			<img src="img/Logos/CHLOGOS1NOstarsHORIZ.png">
-			<p class="banner-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+		<div class="right-quote">
+			<p class="banner-text">"He has a very healthy approach to daily routines and preaches a very important message of maintaining consistency as both a person and as a player."</p>
+			<p class="citation">-Former Student</p>
 		</div>
 	</div>
 
@@ -27,8 +27,8 @@ include(ROOT_PATH . '/inc/header.php');
 		<!-- ************* LISTEN SECTION ************ -->
 
 		<div class="content container grid-6">
-			<a href="#"<h1>Listen</h1></a>
-			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/193055163&amp;color=c08d42&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false"></iframe>
+			<a href="https://soundcloud.com/charlestrombone"><h1>Listen</h1></a>
+			<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/155490088&amp;color=c08d42&amp;auto_play=false&amp;hide_related=false&amp;show_comments=fals&amp;show_user=true&amp;show_reposts=false"></iframe>
 		</div>
 		
 		<!-- ************* About SECTION ************ -->
@@ -40,8 +40,8 @@ include(ROOT_PATH . '/inc/header.php');
 
 		<!-- ************* Watch SECTION ************ -->
 		<div class="content mobile-hide container grid-4">
-			<a href="#"><h1>Watch</h1></a>
-			<iframe src="https://player.vimeo.com/video/69186856?color=8ee59c&portrait=0" width="221" height="166" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>			
+			<a href="https://vimeo.com/charlestrombone"><h1>Watch</h1></a>		
+			<iframe src="https://player.vimeo.com/video/142947952?color=c08d42&portrait=0" width="221" height="166" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 		</div>
 
 		<!-- ************* LESSONS SECTION ************ -->
