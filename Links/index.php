cb630791064f4fc25e6992839fd6cfa8a78d95b7
rmook <richard.mook@gmail.com>
2015-11-25 10:59:08 -0500

    added facebook link

--- Links/index.php
+++ Links/index.php
@@ -11,6 +11,7 @@ include(ROOT_PATH . 'inc/header.php');
 		
 		<div class="content container grid-12">
 			<a href="<?php echo BASE_URL;?>/Links"><h1>Links</h1></a>
+			<h2>Favorite Artists</h2>
 			<p>
 				<a class="feature-link" href="http://www.jayfriedman.net" target="_blank">jayfriedman.net<img src="<?php echo BASE_URL;?>/img/ArrowIcon.png"/></a>
 				Website of long-time Principal Trombone of the Chicago Symphony, Jay Friedman.  Includes informative articles written by Mr. Friedman and his contemporaries.
@@ -19,6 +20,8 @@ include(ROOT_PATH . 'inc/header.php');
 				<a class="feature-link" href="http://www.tobyoft.com" target="_blank">tobyoft.com<img src="<?php echo BASE_URL;?>/img/ArrowIcon.png"/></a>
  				Website of the Principal Trombone of the Boston Symphony Orchestra, Toby Oft.  Here you will find audio and video recording, articles, and a blog by one of the World's premier orchestral trombonists.
  			</p>  
+ 			<h2>Study Materials</h2>
+ 			<p><a class="feature-link" href="http://www.hip-bonestore.com/15_Minute_Warm_Up_Routine_for_Trombone_p/hbm070.htm" target="_blank">The Michael Davis 15-Minute Warm-Up Routine<img src="<?php echo BASE_URL;?>/img/ArrowIcon.png"/></a></p>
 		</div>
 
 <?php
--- inc/footer.php
+++ inc/footer.php
@@ -4,8 +4,8 @@
 	<div class="footer">
 		<p>&copy; 2015 Charles Hopkins<br>Designed &amp; Built by <a href="http://rickmook.com">Rick Mook</a></p>
 		<ul class="social-links">
-			<li class="social-icon"><a href="#"><img src="<?php echo BASE_URL;?>img/facebook.png"></a></li>
-			<li class="social-icon"><a href="#"><img src="<?php echo BASE_URL;?>img/email.png"></a></li>
+			<li class="social-icon"><a href="http://www.facebook.com/charleshopkinstrombone"><img src="<?php echo BASE_URL;?>img/facebook.png"></a></li>
+			<li class="social-icon"><a href="mailto:charles.trombone@gmail.com"><img src="<?php echo BASE_URL;?>img/email.png"></a></li>
 		</ul>
 	</div>
 	<!-- ************ SCRIPTS ************ -->
